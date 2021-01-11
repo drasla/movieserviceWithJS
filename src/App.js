08526e07351bdda28ca46104cd2fa9003b2e3750
Tae.Garam <drasla@naver.com>
2021-01-12 01:44:16 +0900

    2021.01.12 / React propTypes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}) {
+    return (
+        <div>
+        <h1>I like {name}</h1><img src={picture}/>
+        <h4>{rating}/5.0</h4>
+        </div>
+)};
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+};
+
+const foodILike = [
+    {
+        id: 1,
+        name: "Kimchi",
+        image: "a.jpg",
+        rating: 5
+    },
+    {
+        id: 2,
+        name: "Burger",
+        image: "b.jpg",
+        rating: 4.9
+    },
+    {
+        id: 3,
+        name: "Pizza",
+        image: "c.jpg",
+        rating: 5.8
+    },
+    {
+        id: 4,
+        name: "Steak",
+        image: "d.jpg",
+        rating: 1
+    }
+];
+
+function renderFood(dish) {
+    return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {foodILike.map(renderFood)}
+        </div>);
 }
 
 export default App;
