c050505d7049036de4707f36b94b53ba0282df10
Tae.Garam <drasla@naver.com>
2021-01-23 20:30:19 +0900

    #3.0 Class Components and State 완료

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
-    return (
-        <div>
-        <h1>I like {name}</h1><img src={picture}/>
-        <h4>{rating}/5.0</h4>
-        </div>
-)};
+class App extends React.Component {
+    state = {
+        count: 0
+    };
 
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-};
-
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image: "a.jpg",
-        rating: 5
-    },
-    {
-        id: 2,
-        name: "Burger",
-        image: "b.jpg",
-        rating: 4.9
-    },
-    {
-        id: 3,
-        name: "Pizza",
-        image: "c.jpg",
-        rating: 5.8
-    },
-    {
-        id: 4,
-        name: "Steak",
-        image: "d.jpg",
-        rating: 1
+    add = () => {
+        console.log("add");
     }
-];
-
-function renderFood(dish) {
-    return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(renderFood)}
-        </div>);
+    minus = () => {
+        console.log("minus");
+    }
+    render() {
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onclick={this.add}>Add</button>
+                <button onclick={this.minus}>Minus</button>
+            </div>
+        );
+    };
 }
 
 export default App;
