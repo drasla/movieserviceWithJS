d88427c456814903e2a422f9bce3bdd7181d23e4
Tae.Garam <drasla@naver.com>
2021-01-23 23:48:17 +0900

    #4.0 Fetching Movies from API 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
     state = {
         isLoading: true,
         movie: []
     };
+    getMovies = async () => {
+        const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    }
+
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({isLoading: false});
-        }, 6000);
+        this.getMovies();
     }
 
     render() {
