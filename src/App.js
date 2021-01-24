9cb1844c1ed61c92881010afdf78881743db5444
Tae.Garam <drasla@naver.com>
2021-01-24 16:31:40 +0900

    #6.3 Sharing Props Between Routes 완료

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
 import "./App.css";
+import Detail from "./routes/Detail";
 
 function App() {
     return <HashRouter>
         <Navigation />
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/movie-detail" component={Detail} />
     </HashRouter>
 }
 
